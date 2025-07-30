85e1641f570ac2f53ffbd6a416a72bd04484c736
Santi <camelo.santiago9@gmail.com>
2025-07-30 17:21:50 -0500

    Arhivos iniciales de aplicacion

---
+++ src/main/java/com/aluracursosdesafio/frasespeliculas/FrasespeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursosdesafio.frasespeliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrasespeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrasespeliculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursosdesafio/frasespeliculas/FrasespeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursosdesafio.frasespeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasespeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
